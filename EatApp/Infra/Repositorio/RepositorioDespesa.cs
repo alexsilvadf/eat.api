195aea835e4333fdd69acb0c6bf5790f6384cb92
Alex <alex.dasilva@castgroup.com.br>
2025-02-04 14:11:05 -0300

    criando os repositórios generics

---
+++ EatApp/Infra/Repositorio/RepositorioDespesa.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces.IDespesa;
+using Entities.Entidades;
+using Infra.Repositorio.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioDespesa: RepositoryGenerics<Despesa>, InterfaceDespesa
+    {
+    }
+}
