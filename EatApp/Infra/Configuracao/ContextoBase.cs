594c70cd0e9a4e1285a313dc96ab8d97ded53573
Alex <alex.dasilva@castgroup.com.br>
2025-02-04 11:07:41 -0300

    configurando a classe de contexto

---
+++ EatApp/Infra/Configuracao/ContextoBase.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracao
+{
+    public class ContextoBase: IdentityDbContext<ApplicationUser>
+    {
+        public ContextoBase( DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<SistemaFinanceiro> SistemaFinanceiros { get; set; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Despesa> Despesa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return " string de conexão aqui";
+        }
+
+    }
+}
